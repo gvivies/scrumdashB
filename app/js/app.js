5eecc10d230d922012967dfae29761204cf24c48
Gilles Vivies <gilles.vivies@gmail.com>
2014-08-05 21:17:01 +0200

    Fix scrumberry host

--- app/js/app.js
+++ app/js/app.js
@@ -5,7 +5,7 @@ var scrumdashB = angular.module('dashboard', []);
 scrumdashB.controller('JenkinsDashboardCtrl', function ($scope) {
 		
 	$scope.init = function init() {
-		$scope.scrumberryHost = 'http://192.168.1.134:3000';
+		$scope.scrumberryHost = 'http://scrumberryhost:3000';
 	};
 	
 });
