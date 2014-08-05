5fbb2db6969f87fe03ca665a906464abf5d9b7f8
Gilles Vivies <gilles.vivies@gmail.com>
2014-08-05 21:15:23 +0200

    Fix port 8888 -> 3000

--- app/js/app.js
+++ app/js/app.js
@@ -5,7 +5,7 @@ var scrumdashB = angular.module('dashboard', []);
 scrumdashB.controller('JenkinsDashboardCtrl', function ($scope) {
 		
 	$scope.init = function init() {
-		$scope.scrumberryHost = 'http://localhost:8888';
+		$scope.scrumberryHost = 'http://192.168.1.134:3000';
 	};
 	
 });
